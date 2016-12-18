a4d65a3efc8bc5e86413b49cb06dd0d61d2469d1
OysterSandwich <info@fabianos.de>
2016-12-18 23:08:41 +0100

    added default weighting for rules in rule manager

--- src/genetic/rules/RuleManager.java
+++ src/genetic/rules/RuleManager.java
@@ -19,25 +19,39 @@ public class RuleManager {
 		
 		//keep original rules
 		list.add(new OriginalRule());
+		list.get(list.size()-1).weight = 1.f;
 		list.add(new InstrumentsRule());
+		list.get(list.size()-1).weight = 0.7f;
 		list.add(new TicksRule());
+		list.get(list.size()-1).weight = 0.7f;
 		
 		//random rule
 		list.add(new RandomRule());
+		list.get(list.size()-1).weight = 0.1f;
 		
 		//loudness rule
 		list.add(new LoudnessRule());
+		list.get(list.size()-1).weight = 0.5f;
 		
 		//action rules
 		list.add(new VirtuosoRule());
+		list.get(list.size()-1).weight = 0.5f;
 		list.add(new ChromaticRule());
+		list.get(list.size()-1).weight = 1.f;
 		list.add(new HoldsworthRule());
+		list.get(list.size()-1).weight = 1.f;
 		list.add(new LegatoRule());
+		list.get(list.size()-1).weight = 0.5f;
 		list.add(new StaccatoRule());
+		list.get(list.size()-1).weight = 1.f;
 		list.add(new FreeJazzRule());
+		list.get(list.size()-1).weight = 1.f;
 		list.add(new OstinatoRule());
+		list.get(list.size()-1).weight = 1.f;
 		list.add(new WideRule());
+		list.get(list.size()-1).weight = 0.5f;
 		list.add(new PedalRule());
+		list.get(list.size()-1).weight = 1.f;
 		
 		if (Settings.DEBUG)
 			Streams.ruleOut.println("Rules initiated.");
