f616c131872717f9da1ca942ae9faeb72cf8bd97
OysterSandwich <info@fabianos.de>
2017-10-16 13:00:52 +0200

    Introduced map config file for button text to image file mapping

--- src/init/ImageLoader.java
+++ src/init/ImageLoader.java
@@ -1,7 +1,11 @@
 package init;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import javax.swing.ImageIcon;
@@ -10,30 +14,41 @@ import javax.swing.JButton;
 public class ImageLoader {
 	
 	private static final String IMAGE_FOLDER = "./img/"; 
-	public static Map<String, ImageIcon> image = new HashMap<>(); 
+	private static final String MAP_FILENAME = "imgmap";
 	
-	private static final String[] labels = {
-		"delete=delete.png",
-		"save=save.png",
-		"pause=pause.png",
-		"start=play.png",
-		"play=play.png",
-		"stop=stop.png",
-		"record=record.png",
-		"rec=record.png",
-		"resume=resume.png"
-	};
+	private static final String COMMENT_CHAR = "#";
+	
+	public static Map<String, ImageIcon> image = new HashMap<>();
 	
 	static {
-		// write camel case, other cases will be tested (e.g. Delete instead of delete or DELETE)
-		File file;
+		List<String> labels = new LinkedList<String>();
+		File mapFile = new File(IMAGE_FOLDER + MAP_FILENAME);
+		try {
+			RandomAccessFile raf = new RandomAccessFile(mapFile, "r");
+			String line;
+			while ((line = raf.readLine()) != null) {
+				line = line.replaceAll(" ", "");
+				if (line.contains(COMMENT_CHAR) || line.isEmpty())
+					continue;
+				labels.add(line);
+			}
+		} catch (Exception e) {
+			System.err.println("Could not load map file: "+MAP_FILENAME);
+			labels.clear();
+		}
+		
+		File labelFile;
 		for (String label : labels) {
 			String[] labelArray = label.split("=");
-			file = new File(IMAGE_FOLDER+labelArray[1]);
-			if (file.exists())
-				image.put(labelArray[0], new ImageIcon(file.getPath()));
-			else
-				System.err.println("ImageLoader: " + file.getPath() + " does not exist and is not loaded.");
+			if (labelArray.length < 2)
+				System.err.println("Label is not interpretable, skipped: "+label);
+			else {
+				labelFile = new File(IMAGE_FOLDER+labelArray[1]);
+				if (labelFile.exists())
+					image.put(labelArray[0], new ImageIcon(labelFile.getPath()));
+				else
+					System.err.println("ImageLoader: " + labelFile.getPath() + " does not exist and is not loaded.");
+			}
 		}
 	}
 	
