7d5b4794f48f4a6d80ef4287fecbdd07e7408ad8
OysterSandwich <info@fabianos.de>
2017-12-21 14:07:08 +0100

    implemented chord sheet interpreter. Need to extend chord symbol library and implement instruments that play the chords

--- src/gui/ChordSheetFrame.java
+++ src/gui/ChordSheetFrame.java
@@ -2,8 +2,14 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import playback.ChordSheetManager;
@@ -25,11 +31,42 @@ public class ChordSheetFrame extends ManagedFrame {
 		this.setVisible(true);
 	}
 	
-	private JTextArea sheetArea = new JTextArea("sheet area");
-
+	private JTextArea sheetArea = new JTextArea("| c:m7 | f:m7 | c:m7 | % |\n| f:m7 | % | c:m7 | % |\n| g:7 | f:m7 | c:m7 | g:7 |");
+	private JButton editSheetButton = new JButton("Edit");
+	private JButton buildSheetButton = new JButton("Build");
+	
 	private void initGUI() {
 		sheetArea.setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.LIGHT_GRAY));
 		this.add(sheetArea, BorderLayout.CENTER);
+
+		JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		editSheetButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				editSheetButtonClicked();
+			}
+		});
+		buildSheetButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				buildSheetButtonClicked();
+			}
+		});
+		bottomPanel.add(editSheetButton);
+		bottomPanel.add(buildSheetButton);
+		this.add(bottomPanel, BorderLayout.SOUTH);
 	}
 	
+	private void editSheetButtonClicked() {
+		sheetArea.setEnabled(true);
+	}
+	
+	private void buildSheetButtonClicked() {
+		String errorString = chordSheetManager.interpret(sheetArea.getText());
+		if (errorString == ChordSheetManager.INTERPRETING_SUCCESSFUL) {
+			sheetArea.setEnabled(false);
+		} else {
+			JOptionPane.showMessageDialog(this, errorString, "Interpretation failed", JOptionPane.ERROR_MESSAGE);
+		}
+	}
 }
