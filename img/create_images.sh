23c3cb4331c1aad1e543969385420d24d0d153d8
OysterSandwich <info@fabianos.de>
2018-02-08 16:40:46 +0100

    added compiling and running scripts for sh

--- img/create_images.sh
+++ img/create_images.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 if [ $(dpkg-query -W -f='${Status}' librsvg2-bin 2>/dev/null | grep -c "ok installed") -eq 0 ]
 then
 	echo "install package 'librsvg2-bin' with command 'rsvg' to use this install script"
