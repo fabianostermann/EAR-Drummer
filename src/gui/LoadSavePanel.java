f373b30fb2e49d2389f56129b9cc79581d6816a2
OysterSandwich <info@fabianos.de>
2018-02-11 21:19:25 +0100

    load/save/delete is jmenu now for easier use and improved look. Still todo: update list is redundant. Make button?

--- src/gui/LoadSavePanel.java
+++ src/gui/LoadSavePanel.java
@@ -26,15 +26,17 @@ public class LoadSavePanel extends JPanel {
 	private final String SAVE_FOLDER;
 	private final String DEFAULT_FILENAME = "default";
 	
-	private JMenuBar menuBar;
+	private final String defaultSaveName;
+	
 	private JMenu loadMenu = new JMenu("Load");
-	private JTextField saveTagField;
-	private JButton saveButton = ImageLoader.createButton("Save");
-	private JButton deleteButton = ImageLoader.createButton("Delete");
+	private JMenu saveMenu = new JMenu("Save");
+	private JMenu deleteMenu = new JMenu("Delete");
 	private JTextField infoLabel = new JTextField(30);
 	
 	private final LoadSaveable parent;
 	
+	/** holds all instances of this class
+	 * (for loading defaults after startup) */
 	private static final List<LoadSavePanel> memento = new LinkedList<>();
 
 	public static void loadAllDefaultFiles() {
@@ -49,98 +51,101 @@ public class LoadSavePanel extends JPanel {
 	@Override
 	public void setEnabled(boolean enabled) {
 		super.setEnabled(enabled);
-		this.menuBar.setEnabled(enabled);
 		this.loadMenu.setEnabled(enabled);
-		this.saveButton.setEnabled(enabled);
-		this.deleteButton.setEnabled(enabled);
+		this.saveMenu.setEnabled(enabled);
+		this.deleteMenu.setEnabled(enabled);
 	}
 	
-	public LoadSavePanel(LoadSaveable loadSaveable, String saveFolder, String defaultSaveName) {
+	public LoadSavePanel(LoadSaveable loadSaveable, String saveFolder, String pDefaultSaveName) {
 		super(new FlowLayout(FlowLayout.LEFT));
 		
 		memento.add(this);
 		this.parent = loadSaveable;
+		
+		final String defaultSaveName = pDefaultSaveName;
+		this.defaultSaveName = defaultSaveName;
 		
 		File topFolder = new File(TOP_FOLDER);
 		if (!topFolder.exists())
 			topFolder.mkdir();
 		this.SAVE_FOLDER = "./"+TOP_FOLDER+"/"+saveFolder+"/";
 		
-		// TODO generate an unused defaultname number
-		saveTagField = new JTextField(defaultSaveName, 15);
+		// TODO generate an unused default name number
+//		saveTagField = new JTextField(defaultSaveName, 15);
 		
-		menuBar = new JMenuBar();
-		ImageIcon icon;
-		if ((icon = ImageLoader.getImageIcon(loadMenu.getText())) != null) {
+		JMenuBar loadMenuBar = new JMenuBar();
+		ImageIcon loadMenuIcon;
+		if ((loadMenuIcon = ImageLoader.getImageIcon(loadMenu.getText())) != null) {
 			loadMenu.setText(null);
-			loadMenu.setIcon(icon);
+			loadMenu.setIcon(loadMenuIcon);
 		}
-		menuBar.add(loadMenu);
-		updateLoadMenu();
-		saveButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String filename = saveTagField.getText().trim();
-				if(filename.isEmpty()) {
-					infoLabel.setText("Filename is empty.");
-					return;
-				}
-				File saveFile = new File(SAVE_FOLDER + filename);
-				if (saveFile.exists()) {
-					if (0 == JOptionPane.showConfirmDialog(null,
-							"Sure you want to overwrite '"+saveFile.getName()+"'?",
-							"Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE))
-						infoLabel.setText(saveFile.getName()+" overwritten.");
-					else {
-						infoLabel.setText(saveFile.getName()+" not saved.");
-						return;
-					}
-				}
-				else
-					infoLabel.setText(saveFile.getName()+" saved.");
-				
-				try {
-					RandomAccessFile raf = new RandomAccessFile(saveFile, "rw");
-					raf.setLength(0);
-					parent.saveToFile(raf);
-					raf.close();
-				} catch (Exception ex) { ex.printStackTrace(); infoLabel.setText("Error:"+ex.getMessage()); }
-				updateLoadMenu();
-			}
-		});
+		loadMenuBar.add(loadMenu);
 		
-		deleteButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String filename = saveTagField.getText().trim();
-				if (filename.isEmpty()) {
-					infoLabel.setText("Filename is empty.");
-					return;
-				}
-				File deleteFile = new File(SAVE_FOLDER + filename);
-				if (deleteFile.exists()) {
-					if (0 == JOptionPane.showConfirmDialog(null,
-							"Sure you want to delete '"+deleteFile.getName()+"'?",
-							"Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE)) {
-						deleteFile.delete();
-						infoLabel.setText(deleteFile.getName()+" deleted.");
-						updateLoadMenu();
-					}
-					else
-						infoLabel.setText(deleteFile.getName()+" not deleted.");
-				}
-				else
-					infoLabel.setText(deleteFile.getName()+" does not exist.");
-			}
-		});
+		JMenuBar saveMenuBar = new JMenuBar();
+		ImageIcon saveMenuIcon;
+		if ((saveMenuIcon = ImageLoader.getImageIcon(saveMenu.getText())) != null) {
+			saveMenu.setText(null);
+			saveMenu.setIcon(saveMenuIcon);
+		}
+		saveMenuBar.add(saveMenu);
+		
+//		saveMenu.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				
+//				String filename = (String) JOptionPane.showInputDialog(saveMenu,
+//						"Filename to save:", "Save", JOptionPane.QUESTION_MESSAGE,
+//						ImageLoader.getImageIcon("save"), null, defaultSaveName);
+//				
+//				if(filename == null || filename.isEmpty()) {
+//					infoLabel.setText("Filename is empty.");
+//					return;
+//				}
+//				File saveFile = new File(SAVE_FOLDER + filename);
+//				if (saveFile.exists()) {
+//					if (0 == JOptionPane.showConfirmDialog(saveMenu,
+//							"Sure you want to overwrite '"+saveFile.getName()+"'?",
+//							"Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE))
+//						infoLabel.setText(saveFile.getName()+" overwritten.");
+//					else {
+//						infoLabel.setText(saveFile.getName()+" not saved.");
+//						return;
+//					}
+//				}
+//				else
+//					infoLabel.setText(saveFile.getName()+" saved.");
+//				
+//				try {
+//					RandomAccessFile raf = new RandomAccessFile(saveFile, "rw");
+//					raf.setLength(0);
+//					parent.saveToFile(raf);
+//					raf.close();
+//				} catch (Exception ex) { ex.printStackTrace(); infoLabel.setText("Error:"+ex.getMessage()); }
+//				updateAllMenus();
+//			}
+//		});
+		
+		JMenuBar deleteMenuBar = new JMenuBar();
+		ImageIcon deleteMenuIcon;
+		if ((deleteMenuIcon = ImageLoader.getImageIcon(deleteMenu.getText())) != null) {
+			deleteMenu.setText(null);
+			deleteMenu.setIcon(deleteMenuIcon);
+		}
+		deleteMenuBar.add(deleteMenu);
 		
 		infoLabel.setEditable(false);
+		updateAllMenus();
 		
-		this.add(menuBar);
-		this.add(saveTagField);
-		this.add(saveButton);
-		this.add(deleteButton);
+		this.add(loadMenuBar);
+		this.add(saveMenuBar);
+		this.add(deleteMenuBar);
 		this.add(infoLabel);
+	}
+	
+	private void updateAllMenus() {
+		updateLoadMenu();
+		updateSaveMenu();
+		updateDeleteMenu();
 	}
 
 	private void updateLoadMenu() {
@@ -157,7 +162,7 @@ public class LoadSavePanel extends JPanel {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					loadFile(loadFile);
-					updateLoadMenu();
+					updateAllMenus();
 				}
 			});
 			loadMenu.add(item);
@@ -168,10 +173,85 @@ public class LoadSavePanel extends JPanel {
 		item.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				updateLoadMenu();
+				updateAllMenus();
 			}
 		});
 		loadMenu.add(item);
+	}
+	
+	private void updateSaveMenu() {
+		
+		saveMenu.removeAll();
+		
+		File saveFolder = new File(SAVE_FOLDER);
+		if (!saveFolder.exists())
+			saveFolder.mkdir();
+		
+		saveMenu.addSeparator();
+		JMenuItem newFileItem = new JMenuItem("new file");
+		newFileItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveNewFile();
+				updateAllMenus();
+			}
+		});
+		saveMenu.add(newFileItem);
+		
+		saveMenu.addSeparator();
+		for (File file : saveFolder.listFiles()) {
+			JMenuItem item = new JMenuItem(file.getName());
+			final File saveFile = file;
+			item.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					saveExistingFile(saveFile);
+					updateAllMenus();
+				}
+			});
+			saveMenu.add(item);
+		}
+		
+		saveMenu.addSeparator();
+		JMenuItem updateItem = new JMenuItem("update list");
+		updateItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				updateAllMenus();
+			}
+		});
+		saveMenu.add(updateItem);
+	}
+	
+	private void updateDeleteMenu() {
+		
+		deleteMenu.removeAll();
+		
+		File saveFolder = new File(SAVE_FOLDER);
+		if (!saveFolder.exists())
+			saveFolder.mkdir();
+		for (File file : saveFolder.listFiles()) {
+			JMenuItem item = new JMenuItem(file.getName());
+			final File deleteFile = file;
+			item.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					deleteFile(deleteFile);
+					updateAllMenus();
+				}
+			});
+			deleteMenu.add(item);
+		}
+		
+		deleteMenu.addSeparator();
+		JMenuItem item = new JMenuItem("update list");
+		item.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				updateAllMenus();
+			}
+		});
+		deleteMenu.add(item);
 	}
 	
 	private void loadFile(File loadFile) {
@@ -180,11 +260,82 @@ public class LoadSavePanel extends JPanel {
 				RandomAccessFile raf = new RandomAccessFile(loadFile, "r");
 				parent.loadFromFile(raf);
 				raf.close();
-				saveTagField.setText(loadFile.getName());
 				infoLabel.setText(loadFile.getName() + " loaded.");
 			} catch (Exception ex) { ex.printStackTrace(); infoLabel.setText("Error:"+ex.getMessage()); }
 		} else {
 			infoLabel.setText("File '"+loadFile.getName()+"' does not exist.");
+		}
+	}
+	
+	private void saveExistingFile(File saveFile) {
+		if (saveFile.exists()) {
+			try {
+//				if (0 == JOptionPane.showConfirmDialog(saveMenu,
+//						"Sure you want to overwrite '"+saveFile.getName()+"'?",
+//						"Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE))
+//					infoLabel.setText(saveFile.getName()+" overwritten.");
+//				else {
+//					infoLabel.setText(saveFile.getName()+" not saved.");
+//					return;
+//				}
+				RandomAccessFile raf = new RandomAccessFile(saveFile, "rw");
+				parent.saveToFile(raf);
+				raf.close();
+				infoLabel.setText(saveFile.getName() + " saved.");
+			} catch (Exception ex) { ex.printStackTrace(); infoLabel.setText("Error:"+ex.getMessage()); }
+		} else {
+			infoLabel.setText("File '"+saveFile.getName()+"' does not exist.");
+		}
+	}
+	
+	private void saveNewFile() {
+		String filename = (String) JOptionPane.showInputDialog(saveMenu,
+				"Filename to save:", "Save", JOptionPane.QUESTION_MESSAGE,
+				ImageLoader.getImageIcon("save"), null, defaultSaveName);
+		
+		if(filename == null || filename.isEmpty()) {
+			infoLabel.setText("Filename is empty.");
+			return;
+		}
+		File saveFile = new File(SAVE_FOLDER + filename);
+		if (saveFile.exists()) {
+			if (0 == JOptionPane.showConfirmDialog(saveMenu,
+					"Sure you want to overwrite '"+saveFile.getName()+"'?",
+					"Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE))
+				infoLabel.setText(saveFile.getName()+" overwritten.");
+			else {
+				infoLabel.setText(saveFile.getName()+" not saved.");
+				return;
+			}
+		}
+		else
+			infoLabel.setText(saveFile.getName()+" saved.");
+		
+		try {
+			RandomAccessFile raf = new RandomAccessFile(saveFile, "rw");
+			raf.setLength(0);
+			parent.saveToFile(raf);
+			raf.close();
+		} catch (Exception ex) { ex.printStackTrace(); infoLabel.setText("Error:"+ex.getMessage()); }
+		updateAllMenus();
+	}
+	
+	private void deleteFile(File deleteFile) {
+		if (deleteFile.exists()) {
+			try {
+				if (0 != JOptionPane.showConfirmDialog(deleteMenu,
+						"Sure you want to completely delete '"+deleteFile.getName()+"'?",
+						"Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE)) {
+					infoLabel.setText(deleteFile.getName()+" not deleted.");
+					return;
+				}
+				if (deleteFile.delete())
+					infoLabel.setText(deleteFile.getName() + " deleted.");
+				else
+					infoLabel.setText(deleteFile.getName() + " cannot be deleted.");
+			} catch (Exception ex) { ex.printStackTrace(); infoLabel.setText("Error:"+ex.getMessage()); }
+		} else {
+			infoLabel.setText("File '"+deleteFile.getName()+"' does not exist.");
 		}
 	}
 }
