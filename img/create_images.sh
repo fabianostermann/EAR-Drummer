132acae6b39b577208d708110d87724424c7c95e
Fabian Ostermann <fabian.ostermann@udo.edu>
2019-07-01 18:19:40 +0200

    changed svg to png script to match ubuntu18.04 repo

--- img/create_images.sh
+++ img/create_images.sh
@@ -1,11 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ $(dpkg-query -W -f='${Status}' librsvg2-bin 2>/dev/null | grep -c "ok installed") -eq 0 ]
 then
-	echo "install package 'librsvg2-bin' with command 'rsvg' to use this install script"
-	return;
+	echo "install package 'librsvg2-bin' with command 'rsvg-convert' to use this install script"
+	exit 1
 fi
+
+if [ $(dpkg-query -W -f='${Status}' rename 2>/dev/null | grep -c "ok installed") -eq 0 ]
+then
+	echo "install package 'rename'"
+	exit 1
+fi
+
 echo "Generating png out of svg.."
 
-find ./ -name \*.svg -exec rsvg -w 24 -a {} {}.png \;
+find ./ -name "*.svg" -exec rsvg-convert -o {}.png -w 24 -a {} \;
 rename -f 's/.svg.png$/.png/' *.svg.png
+
+exit 0
